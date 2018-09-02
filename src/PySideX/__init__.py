73c3a202ccfadf351e8224398cbe65ec0fd4ca1b
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-09-02 13:22:17 +1200

    Add wrapper code.

---
+++ src/PySideX/__init__.py
@@ -0,0 +1,19 @@
+
+try:
+    import PySide2
+    pyside2_available = True
+except ImportError:
+    pyside2_available = False
+    try:
+        import PySide
+        pyside_available = True
+    except ImportError:
+        pyside_available = False
+        raise ImportError("Neither 'PySide' nor 'PySide2' is available.")
+
+if pyside2_available:
+    from PySide2 import QtCore
+    from PySide2 import QtWidgets
+else:
+    from PySide import QtCore
+    from PySide import QtGui as QtWidgets
